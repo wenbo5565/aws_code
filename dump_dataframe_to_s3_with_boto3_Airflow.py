c7c6554614168ce2cbfced01565083e84a2bf058
Wenbo <wenbo.ma@foxmail.com>
2020-05-13 23:04:21 -0400

    add example script using boto3 and airflow

---
+++ dump_dataframe_to_s3_with_boto3_Airflow.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import datetime
+import boto3
+import pandas as pd
+from io import StringIO
+
+#####################
+# set AWS parameters
+#####################
+aws_access_key_id = ''
+aws_access_key = ''
+bucket = 'wenbo-udacity-eng'
+
+######################
+# create a data frame
+######################
+def create_data_frame():
+    df = pd.DataFrame({'sequence': range(1,10)})
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer)
+    
+    bucket = bucket
+    s3 = boto3.resource('s3', region_name = 'us-west-2', 
+                    aws_access_key_id = aws_access_key_id,
+                    aws_secret_access_key = aws_access_key)
+    
+    s3.Object(bucket, 'airflow_test_from_udacity.csv').put(Body = csv_buffer.getvalue())
+
+
+dag = DAG(
+          'create_df_and_dump_to_s3',
+           start_date = datetime.datetime.now())
+
+create_df_dump_to_s3 = PythonOperator(
+        task_id = 'create_df_and_dump_to_s3',
+        python_callable = create_data_frame,
+        dag = dag
+)
+        
